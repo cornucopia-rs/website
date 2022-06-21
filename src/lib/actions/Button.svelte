cd815f49c37009573973dc242a8968c6496cf1be
Josiane Morissette <jomo.music.123@gmail.com>
2022-06-21 17:00:36 -0400

    💄 Create action components.

---
+++ src/lib/actions/Button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '/static/button_animation.css';
+	export let componentClass = '';
+
+	export let handleClick = () => {};
+</script>
+
+<button on:click={handleClick} class="{componentClass} flex items-center justify-center">
+	<slot>Empty button</slot>
+</button>
---
+++ src/lib/actions/LinkButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Button from '$lib/actions/Button.svelte';
+
+	import '/static/button_animation.css';
+	export let componentClass = '';
+
+	export let href: string;
+	function handleClick() {
+		goto(href);
+	}
+</script>
+
+<Button {handleClick} {componentClass}><slot /></Button>
