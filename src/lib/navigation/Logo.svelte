1496ec48790c2455ddf1b395c08092bdc762eca8
Josiane Morissette <jomo.music.123@gmail.com>
2022-06-21 17:02:15 -0400

    💄 Add the Logo of Cornucopia.

---
+++ src/lib/navigation/Logo.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import LinkButton from '$lib/actions/LinkButton.svelte';
+</script>
+
+<div class="flex-none ">
+	<LinkButton
+		href="/"
+		componentClass=" space-x-1 sm:space-x-4 lg:mr-6 flex items-center tracking-tight sm:tracking-widest"
+	>
+		<img
+			alt="Logo Cornucopia"
+			aria-hidden="true"
+			class=" w-12 h-12 md:w-14 md:h-14 hover:animate-[spin_0.5s_ease_1] "
+			src="/LogoCornucopia.svg"
+		/>
+
+		<div class=" text-4xl font-light sm:hidden lg:block">cornucopia</div>
+	</LinkButton>
+</div>
