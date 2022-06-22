5b85daf7bb2f7bdea61837d6be09e091363afd39
Josiane Morissette <jomo.music.123@gmail.com>
2022-06-21 22:56:24 -0400

    💄 Create a component for Discord and Github links and logos.

---
+++ src/lib/navigation/navBar/DiscordLink.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Discord from '$lib/icons/companies/Discord.svelte';
+	import NavTextButton from './NavTextButton.svelte';
+
+	export let textClass = 'sm:hidden';
+	export let iconSize = 'h-6 w-6 sm:w-10 sm:h-10 ';
+</script>
+
+<NavTextButton href="https://discord.gg/RkQpGVQc">
+	<Discord
+		class="{iconSize} sm:text-base-100 sm:bg-zinc-300 sm:hover:bg-neutral-focus sm:rounded-full sm:p-1"
+	/>
+	<h1 class={textClass}>Discord</h1>
+</NavTextButton>
---
+++ src/lib/navigation/navBar/GithubLink.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Github from '$lib/icons/companies/Github.svelte';
+	import NavTextButton from './NavTextButton.svelte';
+
+	export let textClass = 'sm:hidden';
+	export let iconSize = 'h-6 w-6 sm:w-10 sm:h-10 ';
+</script>
+
+<NavTextButton href="https://github.com/cornucopia-rs/cornucopia">
+	<Github class="{iconSize} sm:hover:text-neutral-focus sm:rounded-full" />
+	<h1 class={textClass}>Github</h1>
+</NavTextButton>
