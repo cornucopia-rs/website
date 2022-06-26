5262d69806e217b6e3c2d9d38503f8f7acd07d72
Josiane Morissette <jomo.music.123@gmail.com>
2022-06-26 19:31:56 -0400

    💄 Replace the svg logo for png.

--- src/lib/actions/DiscordLink.svelte
+++ src/lib/actions/DiscordLink.svelte
@@ -3,15 +3,15 @@
 	import { discordServer } from '$lib/utils/externalLinks';
 
 	export let textClass = 'sm:hidden';
-	export let iconSize = 'h-6 w-6 sm:w-10 sm:h-10 ';
+	export let iconSize = 'h-6 w-6 sm:w-8 sm:h-8 ';
 </script>
 
 <a
-	class="flex items-center space-x-6 tracking-wider text-zinc-100 hover:text-neutral md:px-0 lg:px-4 text-xl md:text-2xl font-normal normal-case "
+	class="flex items-center space-x-6 tracking-widermd:px-0 lg:px-4 text-xl md:text-2xl font-normal normal-case "
 	href={discordServer}
 >
 	<Discord
-		class="{iconSize} opacity-90 hover:text-black text-black bg-white hover:scale-105 rounded-full p-1"
+		class="{iconSize} opacity-80 hover:text-black text-black bg-white hover:scale-105 rounded-full p-1"
 	/>
 	<h1 class={textClass}>Join the Discord</h1>
 </a>
--- src/lib/navigation/Logo.svelte
+++ src/lib/navigation/Logo.svelte
@@ -8,9 +8,9 @@
 			alt="Logo Cornucopia"
 			aria-hidden="true"
 			class=" w-12 h-12 lg:w-14 lg:h-14 hover:animate-[spin_0.5s_ease_1] "
-			src="favicon.svg"
+			src="logo.png"
 		/>
 
-		<div class=" tracking- text-4xl font-light">Cornucopia</div>
+		<div class="text-zinc-200 text-4xl font-light">Cornucopia</div>
 	</LinkButton>
 </div>
